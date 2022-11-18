86a174d787227d07af8bd5c19913058cd3a04d99
Jason Lieb <Jason.lieb@outlook.com>
2022-11-18 11:43:02 -0500

    create html representation of gameboard and style

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+let body = document.querySelector('body');
+let grid = document.createElement('div');
+let button = '<button></button>'
+let buttons = [];
+for(let i = 0; i < 9; i++) {
+  buttons.push(button)
+};
+grid.innerHTML = buttons.join('');
+
+body.appendChild(grid);
+
 let gameboard = (function() {
   return {board: ['', '', '', '', '', '', '', '', '']};
 })();
